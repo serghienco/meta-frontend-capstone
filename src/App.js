78399fe435648cea463bf01e3845e5eaa99141ba
Veaceslav Serghienco <serghienco.v@gmail.com>
2023-11-01 20:13:31 -0500

    added home menu topbar and assets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Topbar from './components/Topbar';
+
 
 function App() {
-  return <div className="App">Home Page</div>;
+  return (
+    <div>
+      <Topbar />
+      <div className="App">Home Page</div>;
+    </div>
+  );
 }
 
 export default App;
